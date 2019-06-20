d5eb201255845197d5fdfc8111160c49e6520b35
Leonardo Motta Maia <>
2019-06-19 22:09:48 -0300

    mods to petsc compilation

--- compile_petsc.sh
+++ compile_petsc.sh
@@ -5,9 +5,12 @@ FC=/usr/bin/mpif90
 CXX=/usr/bin/mpicxx
 
 # HDF5 compiled with fortran and parallel support.
+# FC=/usr/bin/mpif90 CC=/usr/bin/mpicc CXX=/usr/bin/mpicxx ./configure --prefix=/home/leonardo/opt/hdf5/ --enable-parallel --enable-fortran --enable-cxx --enable-unsupported
 HDF5_DIR=/home/leonardo/opt/hdf5
 
-CC=$CC FC=$FC ./configure --prefix=/home/leonardo/opt/petsc --with-scalar-type=complex --with-cc=$CC --with-fc=$FC --with-hdf5=1 --with-hdf5-dir=$HDF5_DIR --with-fortran-bindings=1 --with-cgns=1 --with-cxx=$CXX --with-blaslapack=1 --with-blas-lib=/usr/lib/libblas.so --with-lapack-lib=liblapack.so
+# The CGNS library compiled in my home opt folder is not working with the linker.
+
+CXX=$CXX CC=$CC FC=$FC ./configure --prefix=/home/leonardo/opt/petsc --with-scalar-type=complex --with-cc=$CC --with-fc=$FC --with-hdf5=1 --with-hdf5-dir=$HDF5_DIR --with-fortran-bindings=1 --with-cgns=1 --with-cxx=$CXX --with-blaslapack=1 --with-blas-lib=/usr/lib/libblas.so --with-lapack-lib=liblapack.so
 
 # Petsc will dump some instructions, so follow them ! 
 # Note: Now able to install the code in a custom prefix
