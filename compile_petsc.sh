2329ee7e314f5825d97bc1379d35c22d49ae026d
Leonardo Motta Maia <>
2019-06-20 17:21:18 -0300

    Adding valgrind

--- compile_petsc.sh
+++ compile_petsc.sh
@@ -10,7 +10,7 @@ export HDF5_DIR=/home/leonardo/opt/hdf5
 
 # The CGNS library compiled in my home opt folder is not working with the linker.
 
-CXX=$CXX CC=$CC FC=$FC ./configure --prefix=/home/leonardo/opt/petsc --with-scalar-type=complex --with-cc=$CC --with-fc=$FC --with-hdf5=1 --with-hdf5-dir=$HDF5_DIR --with-fortran-bindings=1 --with-cgns=1 --with-cxx=$CXX --with-blaslapack=1 --with-blas-lib=/usr/lib/libblas.so --with-lapack-lib=liblapack.so
+CXX=$CXX CC=$CC FC=$FC ./configure --prefix=/home/leonardo/opt/petsc --with-scalar-type=complex --with-cc=$CC --with-fc=$FC --with-hdf5=1 --with-hdf5-dir=$HDF5_DIR --with-fortran-bindings=1 --with-cgns=1 --with-cxx=$CXX --with-blaslapack=1 --with-blas-lib=/usr/lib/libblas.so --with-lapack-lib=liblapack.so --with-valgrind=1
 
 # Petsc will dump some instructions, so follow them ! 
 # Note: Now able to install the code in a custom prefix
